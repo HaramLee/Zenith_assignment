0c99d9be6837473592a6cf98fe2a97c2ce8e2bbb
Jordan Hamade <ardf2014@hotmail.com>
2016-10-20 10:42:05 -0700

    removed about and contact page
    Signed-off-by: Jordan Hamade <ardf2014@hotmail.com>

--- Zenith_V2/ZenithWebSite/Controllers/HomeController.cs
+++ Zenith_V2/ZenithWebSite/Controllers/HomeController.cs
@@ -59,19 +59,5 @@ namespace ZenithWebSite.Controllers
             return View(SortedList);
         }
 
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
